dd7f06184b568c93c6f092abc3c7357e58158100
Alex McBride <Alex.N.McBride@gmail.com>
2026-02-16 17:37:37 -0700

    checking monthly regression

---
+++ analysis/monthly_zone_coefs.m
@@ -0,0 +1,12 @@
+out="/auto/home/mcbride/Amazon-Scatterometry/data/monthly/z%d_monthly_coefs.mat";
+coefs=zeros(5,12,2);
+for i=2:6
+    m_coefs=zeros(12,2);
+    for j=1:12
+        m_coefs(j,:)=anoms_by_month_zone(i,j);
+    end
+    coefs(i-1,:,:)=m_coefs;
+    output=sprintf(out,i);
+    % save(output,"m_coefs");
+end
+
